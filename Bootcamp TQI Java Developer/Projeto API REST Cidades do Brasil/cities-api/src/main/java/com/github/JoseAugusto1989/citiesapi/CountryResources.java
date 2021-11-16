2753c3411a1122d3a811decb06795f39f3dd441b
Jose Augusto de Oliveira Pins <64021880+JoseAugusto1989@users.noreply.github.com>
2021-11-15 21:22:07 -0300

    atualizando database

--- Bootcamp TQI Java Developer/Projeto API REST Cidades do Brasil/cities-api/src/main/java/com/github/JoseAugusto1989/citiesapi/CountryResources.java
+++ Bootcamp TQI Java Developer/Projeto API REST Cidades do Brasil/cities-api/src/main/java/com/github/JoseAugusto1989/citiesapi/CountryResources.java
@@ -1,11 +1,13 @@
 package com.github.JoseAugusto1989.citiesapi;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/countries")
@@ -23,6 +25,20 @@ public class CountryResources {
     @GetMapping
     public List<Country> countries() {
         return repository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Country> getOne(@PathVariable long id) {
+        Optional<Country> optional = Optional.of(repository.getById(id));
+
+        if (optional.isPresent()) {
+            return ResponseEntity.ok().body(optional.get());
+
+        } else {
+            return ResponseEntity.notFound().build();
+
+        }
+
     }
 
 }
